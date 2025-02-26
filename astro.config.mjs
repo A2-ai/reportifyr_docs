c4de9f5b986f3fa7c3171a1f7e87e4e4e1b4d62c
jacobdum <jacob@a2-ai.com>
2025-02-26 16:48:43 +0000

    Set-up for Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://literate-adventure-1pnpg8r.pages.github.io/reportifyr_docs",
+	base: "/reportifyr_docs",
   markdown: {
     rehypePlugins: [
       rehypeSlug,
